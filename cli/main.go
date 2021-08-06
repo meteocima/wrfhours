4a21e2c2c9ef467ef7d74928de385908d5ecf373
Andrea Parodi <andrea@parro.it>
2021-08-06 12:52:27 +0200

    completed cli

--- cli/main.go
+++ cli/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+
+	"github.com/meteocima/wrfoutput"
 )
 
 // Version of the command
@@ -13,6 +17,34 @@ func main() {
 	flag.Parse()
 	if showver != nil && *showver {
 		fmt.Printf("wrfhours ver. %s\n", Version)
-		return
+		os.Exit(0)
 	}
+
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	parser := wrfoutput.NewParser()
+
+	go parser.Parse(os.Stdin)
+
+	for file := range parser.Results.Files {
+		buff, err := json.Marshal(file)
+		if err != nil {
+			return err
+		}
+
+		if _, err = fmt.Fprintln(os.Stdout, string(buff)); err != nil {
+			return err
+		}
+	}
+
+	if err := <-parser.Results.Errs; err != nil {
+		return err
+	}
+
+	return nil
 }
